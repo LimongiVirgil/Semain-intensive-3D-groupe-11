88bd6bfb5d0069460aff403f146bc53a33e2c88b
LimongiVirgil <limongi.virgil@gmail.com>
2019-04-18 14:18:03 +0200

    fix some bug

---
+++ src/js/pop-up.js
@@ -0,0 +1,14 @@
+
+    var outButton = document.getElementById('#aide');
+    var main = document.getElementById("pop-up-1");
+    var hide = document.getElementById("arrow-pop");
+    
+    function simulator_FB (){
+      "use strict"
+      main.style.display = "block";
+    }
+    
+    function simulator_FB_close (){
+      "use strict"
+      main.style.display = "none";
+    }
